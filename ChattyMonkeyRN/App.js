2c67b0c7fd962b01a576c3198bb13c7d8adf266d
Prakash Manandhar <jsaaymi@gmail.com>
2019-10-15 23:28:46 -0400

    Removed bug where setY is called twice

--- ChattyMonkeyRN/App.js
+++ ChattyMonkeyRN/App.js
@@ -26,44 +26,26 @@ function wait(timeout) {
 
 const App: () => React$Node = () => {
 
-  const [refreshing, setRefreshing] = React.useState(false);
   const [y, setY] = React.useState(100);
-  const [ , updateState] = React.useState();
-  const forceUpdate = React.useCallback(
-    () => updateState({}),[],
-  );
-
+  
   useEffect(() => {
-    console.log('Reached HEre-------')
+    //console.log('Reached HEre-------')
     setTimeout(() => {
-      console.log('Reached HEre-------Settimeout')
-      setY(y + 100);
-      if(y > 500) setY(100);
-      forceUpdate();
-    }, 200)
+      console.log('Reached HEre-------Settimeout ' + y)
+      if(y > 500) {
+        setY(100);
+      } else {
+        setY(y + 1);
+      }      
+    }, 10)
   })
   
-  requestAnimationFrame(() => {
-    setY(y + 10);
-    if(y > 500) setY(100);
-  });
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    setY(y + 100);
-    if(y > 500) setY(100);
-    wait(200).then(() => setRefreshing(false));
-  }, [refreshing]);
-
   return (
     <>
       {/* <GameEngine style={styles.container}>
            <StatusBar hidden={true} />
       </GameEngine> */}
-      <ScrollView style={styles.container}
-      refreshControl={
-      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }>
+      <ScrollView style={styles.container}>
         <View style={{width: 50, height: 50, backgroundColor: 'brown', top: 500}} />
         <View style={{width: 100, height: 50, backgroundColor: 'brown', top: y}} />
       </ScrollView>
