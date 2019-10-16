c53d3153eab07fe64ac88ae894ccd486fbbdbe83
Sudha Bhattarai <sudha.bhattarai@gmail.com>
2019-10-15 23:06:16 -0400

    Add first sprite

--- ChattyMonkeyRN/App.js
+++ ChattyMonkeyRN/App.js
@@ -6,25 +6,67 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   StatusBar,
   StyleSheet,
-  View
+  View,
+  RefreshControl,
+  ScrollView
 } from 'react-native';
 
 import Matter from "matter-js";
 import { GameEngine } from "react-native-game-engine";
 
+function wait(timeout) {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+}
+
 const App: () => React$Node = () => {
+
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [y, setY] = React.useState(100);
+  const [ , updateState] = React.useState();
+  const forceUpdate = React.useCallback(
+    () => updateState({}),[],
+  );
+
+  useEffect(() => {
+    console.log('Reached HEre-------')
+    setTimeout(() => {
+      console.log('Reached HEre-------Settimeout')
+      setY(y + 100);
+      if(y > 500) setY(100);
+      forceUpdate();
+    }, 200)
+  })
+  
+  requestAnimationFrame(() => {
+    setY(y + 10);
+    if(y > 500) setY(100);
+  });
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setY(y + 100);
+    if(y > 500) setY(100);
+    wait(200).then(() => setRefreshing(false));
+  }, [refreshing]);
+
   return (
     <>
       {/* <GameEngine style={styles.container}>
            <StatusBar hidden={true} />
       </GameEngine> */}
-      <View style={styles.container}>
-      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      </View>
+      <ScrollView style={styles.container}
+      refreshControl={
+      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+        <View style={{width: 50, height: 50, backgroundColor: 'brown', top: 500}} />
+        <View style={{width: 100, height: 50, backgroundColor: 'brown', top: y}} />
+      </ScrollView>
     </>
   );
 };
@@ -34,10 +76,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'green',
   },
-  level1: {
-    backgroundColor: 'brown',
-    display: 'flex',
-    height: '60px',
+  scrollView: {
+    flex: 0.1,
   }
 });
 
