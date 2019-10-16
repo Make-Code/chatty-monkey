ae6ceb851811a3275f2297d298d40712704f5609
Prakash Manandhar <jsaaymi@gmail.com>
2019-10-16 01:23:06 -0400

    Horizontal scrolling platform done :)

--- ChattyMonkeyRN/App.js
+++ ChattyMonkeyRN/App.js
@@ -12,54 +12,85 @@ import {
   StyleSheet,
   View,
   RefreshControl,
-  ScrollView
+  ScrollView,
+  Dimensions, Div
 } from 'react-native';
+
+function getPlatformsWithOffset(
+      screenWidth, 
+      platformWidth, 
+      offset) 
+{
+  let x = platformWidth - offset;
+  const blocks = [[0, x]];
+  while((x + platformWidth) < screenWidth)
+  {
+    blocks.push([x, x + platformWidth]);
+    x = x + platformWidth;
+  }
+  blocks.push([x, screenWidth]);
+  return blocks;
+}
 
 const App: () => React$Node = () => {
 
   const PLATFORM_HEIGHT = 50;
-  const PLATFORM_WIDTH  = 200;
-  const PLATFORM_DIFF = 100;
+  const PLATFORM_WIDTH  = 100;
+  const PLATFORM_DIFF = 200;
   const [y, setY] = React.useState(100);
   const [t, setT] = React.useState(0);
   
   useEffect(() => {
     //console.log('Reached HEre-------')
     let timerHandle = setTimeout(() => {
-      console.log('Reached HEre-------Settimeout ' + y)
+      //console.log('Reached HEre-------Settimeout ' + y)
       if(y > 500) {
         setY(100);
       } else {
         setY(y + 1);
       }    
       setT(t + 1);  
-    }, 10);
+    }, 20);
     return function cleanup() {
       clearTimeout(timerHandle);
     };
   });
 
-  const targetRef = useRef();
-  const [dimensions, setDimensions] = useState({ width:0, height: 0 });
+  const screen = Dimensions.get('screen');
 
-  useLayoutEffect(() => {
-    if (targetRef.current) {
-      setDimensions({
-        width: targetRef.current.offsetWidth,
-        height: targetRef.current.offsetHeight
-      });
+  const offset = t%(screen.width);
+  const blocks = getPlatformsWithOffset(
+    screen.width, PLATFORM_WIDTH, offset);
+  let blockUp = false;
+  let block_width = 0;
+  let block_y = 0;
+  const output_list = [];
+  for (var i = 0; i < blocks.length; i++)
+  {
+    blockUp = !blockUp;
+    block_width = blocks[i][1] - blocks[i][0];
+    if (blockUp) {
+      block_y = 160 + PLATFORM_DIFF;
+    } else {
+      block_y = 160;
     }
-  }, []);
+    output_list.push (
+      <View key={i} style={{
+        height: PLATFORM_HEIGHT, 
+        backgroundColor: blockUp?'brown':'blue', 
+        top: block_y,
+        left : blocks[i][0],
+        width: block_width,
+        position: 'absolute'}} />
+    );
+    //console.log(blocks[i])
+  }
 
   return (
       <ScrollView style={styles.container}>
         <View style={{width: 50,  height: PLATFORM_HEIGHT, 
-                      backgroundColor: 'yellow', top: 500}} />
-        <View style={{width: PLATFORM_WIDTH, height: PLATFORM_HEIGHT, 
-                      backgroundColor: 'brown', top: y}} />
-        <View style={{width: PLATFORM_WIDTH, height: PLATFORM_HEIGHT, 
-                      backgroundColor: 'blue', top: y + PLATFORM_DIFF,
-                      left: 20}} />
+                      backgroundColor: 'yellow', top: y}} />
+        {output_list}
       </ScrollView>
   );
 };
@@ -67,7 +98,7 @@ const App: () => React$Node = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: 'green', 
   }
 });
 
